4f8c66f58c155cee8b795e7771b86c3ee92fd4e6
Aydin <aydin@kindyhub.com.au>
2018-06-19 16:46:48 +1000

    Added Todos model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-use Faker\Generator as Faker;
+use Illuminate\Database\Seeder;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,11 +13,17 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+    ];
+});
+$factory->define(App\Todo::class, function (Faker\Generator $faker) {
+    return [
+        'task' => $faker->sentence,
+        'done' => rand(0,1),
     ];
 });
